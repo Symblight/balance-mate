ee85fbd56257fa69d27c83f8904f15ea3fc990ab
Aleksei Tkachenko <symblight@gmail.com>
2023-12-29 22:04:45 +0200

    add spin

--- packages/ui/lib/components/index.ts
+++ packages/ui/lib/components/index.ts
@@ -1,3 +1,4 @@
 export { default as PvButton } from "./button/button.ts";
 export { default as PvInput } from "./input/input.ts";
 export { default as PvIcon } from "./icon/icon.ts";
+export { default as PvSpin } from "./spin/spin.ts";
