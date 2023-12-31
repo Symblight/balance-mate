ff2df2153721243f5eb9383b17674900f87ccfc5
Aleksei Tkachenko <symblight@gmail.com>
2023-12-31 10:42:19 +0200

    add dialog

---
+++ packages/ui/lib/components/dialog/stories/dialog.stories.ts
@@ -0,0 +1,56 @@
+import { html } from "lit";
+import { Meta, StoryObj } from "@storybook/web-components";
+
+import "../../../styles/theme.css";
+import "../dialog.ts";
+import "../../button/button.ts";
+import "../../input/input.ts"
+
+import type PvDialogProps from "../dialog.ts";
+
+function Template({ open }: PvDialogProps) {
+  function handleToggle() {
+    const dialog = document.querySelector("pv-dialog");
+
+    if (!dialog) return;
+
+    if (!dialog.open) {
+      dialog.show();
+    } else {
+      dialog.close();
+    }
+  }
+  return html`
+    <pv-button @click=${handleToggle}>Open</pv-button>
+    <pv-dialog headerTitle="Dialog test">
+      <div>
+        <form>
+          Hello world
+          <pv-input></pv-input>
+        </form>
+      </div>
+      <div slot="footer">
+        <pv-button @click=${handleToggle}>Submit</pv-button>
+        <pv-button @click=${handleToggle} variant="secondary">Cancel</pv-button>
+      </div>
+    </pv-dialog>
+  `;
+}
+
+const meta = {
+  title: "Dialog",
+  component: "pv-dialog",
+  tags: ["autodocs"],
+  render: Template,
+  argTypes: {
+    open: {
+      control: { type: "boolean" },
+    },
+  },
+} satisfies Meta<PvDialogProps>;
+export default meta;
+
+type Story = StoryObj<PvDialogProps>;
+export const Regular: Story = {
+  args: {},
+};
